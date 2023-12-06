4aee6b99cca711c7a5ebec715fbc1d15128dad7c
Memby Poudel <officialmembypoudel@gmail.com>
2023-12-06 21:46:54 +0545

    First Commit

---
+++ Excercise5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Excercise5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Running, Program!");
+
+        Student std = new Student("John Doe", 20, "Computer Science");
+
+        std.Introduce();
+
+        Console.ReadKey();
+    }
+}
+
---
+++ Excercise5/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Excercise5
+{
+	public class Student
+	{
+		private string name;
+		private int age;
+		private string major;
+
+		public Student(string name, int age, string major)
+		{
+			this.name = name;
+			this.age = age;
+			this.major = major;
+		}
+
+		public void Introduce()
+		{
+			Console.WriteLine($"Hello! This is {name} of age {age}, studying {major}.");
+		}
+	}
+}
+
